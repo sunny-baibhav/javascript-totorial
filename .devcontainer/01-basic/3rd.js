5c357ff9348217145997e6a020d0f8a2596953a5
sunny baibhav <147418719+sunny-baibhav@users.noreply.github.com>
2024-01-07 13:12:48 +0000

    container

---
+++ .devcontainer/01-basic/3rd.js
@@ -0,0 +1,16 @@
+let Std_name="sunny baibhav"
+let age=21
+let ID=null
+let Islogin =0
+let state="punjab"
+console.log(typeof ID)
+console.log(typeof age)
+
+//datatypes  of javascript
+
+// string=        "hiii, hello..."
+// boolean=        true/ false
+// number=         0,1,2..
+// symbol =         unique
+// undefine =       undefine
+// null =            object
