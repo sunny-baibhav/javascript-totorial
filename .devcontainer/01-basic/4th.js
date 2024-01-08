32570e2688fa5cbd3a2d2c2035675e082ffadc6d
sunny baibhav <147418719+sunny-baibhav@users.noreply.github.com>
2024-01-08 17:12:33 +0000

    container

---
+++ .devcontainer/01-basic/4th.js
@@ -0,0 +1,32 @@
+
+// conversion operations
+
+let score=33
+console.log(typeof(score))
+
+let valueInNumber= Number(score)
+console.log(typeof(valueInNumber))
+
+//NaN is known as not a number
+
+let bp=144
+console.log(typeof(bp))
+
+let valueInString = String(bp)
+console.log(typeof(valueInString))
+
+let wicket = null
+console.log(typeof(wicket))
+let valueintable = Number(wicket)
+console.log(typeof(valueintable))
+
+let isLoggedIn = 1
+console.log(typeof(isLoggedIn))
+
+let IsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof(IsLoggedIn)) 
+
+// boolean conversion
+// true = 1, false = 0
+// "" = false
+// "sunny" = true
