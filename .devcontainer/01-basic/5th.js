905e26581f5d4972bccaf4046471cd302ede1921
sunny baibhav <147418719+sunny-baibhav@users.noreply.github.com>
2024-01-11 16:24:53 +0000

    container

---
+++ .devcontainer/01-basic/5th.js
@@ -0,0 +1,20 @@
+// conditionals statements
+// if or if else or if else if 
+
+let age =20;
+if(age == 20){
+    console.log("you can enter in my room")
+}
+if(age<=20){
+    console.log("you cannot  enter in my room")
+}
+
+// if else
+let rank = 80
+if(rank == 80){
+    console.log("you can take addimission")
+}
+else{
+    
+    console.log("addimission not allowed")
+}
